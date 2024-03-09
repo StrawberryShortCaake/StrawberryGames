05a2c7aa144b925c04dcf7eb165cac7e764846b3
StrawberryShortCaake <159228549+StrawberryShortCaake@users.noreply.github.com>
2024-03-08 20:07:38 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+document.getElementsByClassName("article")[0].addEventListener("click", function(){
+    window.open("https://dorkmurderer.github.io/connect4/")
+})
+
+document.getElementsByClassName("article")[1].addEventListener("click", function(){
+    window.open("https://dorkmurderer.github.io/memorygame/")
+})
+
+document.getElementsByClassName("article")[2].addEventListener("click", function(){
+    window.open("https://dorkmurderer.github.io/CatchADratini/")
+})
+
+document.getElementsByClassName("article")[3].addEventListener("click", function(){
+    window.open("https://strawberryshortcaake.github.io/snakegame/")
+})
+
+document.getElementsByClassName("article")[4].addEventListener("click", function(){
+    window.open("https://strawberryshortcaake.github.io/pixelart/")
+})
+document.getElementsByClassName("article")[5].addEventListener("click", function(){
+    window.open("https://dorkmurderer.github.io/ChildrensMathGame/")
+})
